928d0b97a5c7ad3847a52627b5a997a43b10570b
reponche <n.reponche@gmail.com>
2019-11-04 22:10:30 +0300

    Changed test

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import unittest
 from get_temp import conv_farh_to_cels
-from get_temp import add_C
 
 class TestGetTemp(unittest.TestCase):
 
-    def test_conv_farh_to_cels(self):
-        self.assertEqual(conv_farh_to_cels(50), 10.0)
+    fahr = 50
 
-    def test_add_C(self):
-        self.assertEqual(add_C(50), "50℃")
+    def test_conv_farh_to_cels(fahr):
+        cels = conv_farh_to_cels(fahr)
+        self.assertEqual(conv_farh_to_cels(fahr, cels)
 
-if __name__ == '__main__':
+
+if __name__ == '__main__' :
     unittest.main()
