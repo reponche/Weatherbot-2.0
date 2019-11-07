316fdbb7a3cf441852edcddc0141b10390338707
reponche <n.reponche@gmail.com>
2019-11-07 12:28:55 +0300

    Edited functions

--- get_temp.py
+++ get_temp.py
@@ -1,9 +1,15 @@
 import requests
 
 def get_weather():
-    url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    url = "https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=da161f5c79219dcf1ea9677481ddd49a"
     response = requests.get(url).json()
-    fahr = int(response["query"]["results"]["channel"]["item"]["condition"]["temp"])
+    # temp = int(response["coord"]["lon"]["channel"]["item"]["condition"]["temp"])
+    return response
+
+def conv_farh_to_cels(fahr):
     cels = ((fahr - 32) * 5/9)
-    temp = cels + "°C"
+    return cels
+
+def add_C(cels):
+    temp = str(conv_farh_to_cels(cels)) + "℃"
     return temp
