17537e56ca8b17b922e99f0d2e040855b93fefa6
reponche <n.reponche@gmail.com>
2019-11-09 00:23:39 +0300

    Edited add_C

--- get_temp.py
+++ get_temp.py
@@ -1,10 +1,11 @@
 import requests
 
-# def get_weather():
-    # url = "https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=da161f5c79219dcf1ea9677481ddd49a"
-    # response = requests.get(url).json()
-    # temp = int(response["coord"]["lon"]["channel"]["item"]["condition"]["temp"])
-    # return response
+#def get_weather():
+#    url = "https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=da161f5c79219dcf1ea9677481ddd49a"
+#    response = requests.get(url).json()
+#    temp = int(response["coord"]["lon"]["channel"]["item"]["condition"]["temp"])
+#    return response
+
 fahr = 50
 
 def conv_farh_to_cels(fahr):
@@ -14,7 +15,7 @@ def conv_farh_to_cels(fahr):
 cels = str(conv_farh_to_cels(fahr))
 
 def add_C(cels):
-    temp = add_C(cels) + "C"
+    temp = cels + "C"
     return temp
 
 print(add_C(cels))
