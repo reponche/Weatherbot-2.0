25abf1c6b18fa9db1df119662590f96ba6a99542
reponche <n.reponche@gmail.com>
2019-11-15 20:29:05 +0300

    Replaced test_conv_fahr_to_cels to test_conv_kelv_to_cels

--- tests/test_get_temp.py
+++ tests/test_get_temp.py
@@ -8,10 +8,10 @@ class TestGetTemp(unittest.TestCase):
         a = get_weather("Moscow", OWN_TOKEN)
         self.assertIsInstance(a, int)
 
-    def test_conv_farh_to_cels(self):
-        fahr = 50
-        cels = conv_farh_to_cels(fahr)
-        self.assertEqual(cels, 10.0)
+    def test_conv_kelv_to_cels(self):
+        kelv = 70
+        cels = conv_kelv_to_cels(kelv)
+        self.assertEqual(cels, -203)
 
     def test_add_C(self):
         cels = "10"
