a5816f9c271d5688c11694dafc11fe028500744e
reponche <n.reponche@gmail.com>
2019-11-15 19:32:26 +0300

    Replaced conv_fahr_to_cels() to conv_kelv_to_cels()

--- weatherbot/get_temp.py
+++ weatherbot/get_temp.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python
-
 import requests
+from weatherbot.tokens import OWN_TOKEN
 
-def get_weather(city, api_token):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={},RU&appid={}".format(city, api_token)
+city = "Moscow"
+
+def get_weather(city, OWN_TOKEN):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={},RU&appid={}".format(city, OWN_TOKEN)
     response = requests.get(url).json()
     temp = int(response["main"]["temp"])
     return temp
 
-def conv_farh_to_cels(fahr):
-    cels = int((fahr - 32) * 5/9)
+def conv_kelv_to_cels(temp):
+    cels = int(temp - 273.15)
     return round(cels)
 
 def add_C(cels):
     return (cels + "°C").encode('utf-8')
+
+print(conv_kelv_to_cels(70))
