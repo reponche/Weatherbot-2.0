572491caef688dbae6dafa7bfe0bc7f9881057df
reponche <n.reponche@gmail.com>
2020-01-03 21:07:08 +0300

    Fix test_puller variables

--- tests/test_puller.py
+++ tests/test_puller.py
@@ -13,8 +13,8 @@ class TestPuller(unittest.TestCase):
     def test_get_new(self):
         updates = [{"message": "/start","id": 1},{"message": "London, UK","id": 2},{"message": "/weather","id": 3},{"message": "message","id": 4},{"message": "Moscow","id": 5}]
         expectation = [{"message": "/weather","id": 3}, {"message": "message","id": 4}, {"message": "Moscow","id": 5}]
-        self.assertEqual(get_new(elems, 2), expectation)
-        self.assertEqual(get_new(elems, 5), [])
+        self.assertEqual(get_new(updates, 2), expectation)
+        self.assertEqual(get_new(updates, 5), [])
 
 if __name__ == "__main__" :
     unittest.main()
