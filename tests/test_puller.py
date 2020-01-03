10efee923bceca174080de4c2b3d69f9afb1db4c
reponche <n.reponche@gmail.com>
2020-01-03 21:04:07 +0300

    Fix puller functions

--- tests/test_puller.py
+++ tests/test_puller.py
@@ -11,9 +11,9 @@ class TestPuller(unittest.TestCase):
         self.assertEqual(should_add({"message": "Foo", "id": 3}, 4), False)
 
     def test_get_new(self):
-        elems = [{"message": "/start","id": 1},{"message": "London, UK","id": 2},{"message": "/weather","id": 3},{"message": "message","id": 4},{"message": "Moscow","id": 5}]
-        test_elems = [{"message": "/weather","id": 3}, {"message": "message","id": 4}, {"message": "Moscow","id": 5}]
-        self.assertEqual(get_new(elems, 2), test_elems)
+        updates = [{"message": "/start","id": 1},{"message": "London, UK","id": 2},{"message": "/weather","id": 3},{"message": "message","id": 4},{"message": "Moscow","id": 5}]
+        expectation = [{"message": "/weather","id": 3}, {"message": "message","id": 4}, {"message": "Moscow","id": 5}]
+        self.assertEqual(get_new(elems, 2), expectation)
         self.assertEqual(get_new(elems, 5), [])
 
 if __name__ == "__main__" :
