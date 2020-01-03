78cd252461b881ee12735a24c25fb030f796f23b
reponche <n.reponche@gmail.com>
2020-01-03 18:17:32 +0300

    Fixed mistakes

--- tests/puller_tests.py
+++ tests/puller_tests.py
@@ -1,20 +1,20 @@
 import unittest
+
 from weatherbot.puller import should_add, get_new
 
-class TestGetTemp(unittest.TestCase):
+class TestPuller(unittest.TestCase):
 
     def test_should_add_true(self):
         self.assertEqual(should_add({"message": "Foo", "id": 3}, 1), True)
 
     def test_should_add_false(self):
-        a = should_add({"message": "Foo", "id": 3}, 4)
-        self.assertEqual(a, False)
+        self.assertEqual(should_add({"message": "Foo", "id": 3}, 4), False)
 
     def test_get_new(self):
-        dict = [{"message": "/start","id": 1},{"message": "London, UK","id": 2},{"message": "/weather","id": 3},{"message": "message","id": 4},{"message": "Moscow","id": 5}]
-        test_res = '{"message": "/weather","id": 3} {"message": "message","id": 4} {"message": "Moscow","id": 5}'
-        self.assertEqual(get_new(dict, 2), test_res)
-        self.assertEqual(get_new(dict, 5), None)
+        elems = [{"message": "/start","id": 1},{"message": "London, UK","id": 2},{"message": "/weather","id": 3},{"message": "message","id": 4},{"message": "Moscow","id": 5}]
+        test_elems = [{"message": "/weather","id": 3}, {"message": "message","id": 4}, {"message": "Moscow","id": 5}]
+        self.assertEqual(get_new(elems, 2), test_elems)
+        self.assertEqual(get_new(elems, 5), [])
 
 if __name__ == "__main__" :
     unittest.main()
