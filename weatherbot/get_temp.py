31d94afbbb440983f931c6d72f83130079ae74c8
Anton Parkhomenko <mailbox@chuwy.me>
2020-01-03 19:21:14 +0700

    Clean-up get_temp

--- weatherbot/get_temp.py
+++ weatherbot/get_temp.py
@@ -1,16 +1,16 @@
 import requests
+
 from weatherbot.tokens import OWN_TOKEN
 
+OWM_URL = "https://api.openweathermap.org/data/2.5/weather?q={},RU&appid={}"
+
 def get_weather(city):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={},RU&appid={}".format(city, OWN_TOKEN)
+    url = OWM_URL.format(city, OWN_TOKEN)
     response = requests.get(url).json()
     return response
 
-#если город в сервисе openweathermap не находится, то сервис выдает словарь, в котором говорится о том, что сервис не нашел такого города
-#структура словаря перестраивается и ф-ия get_temp не может найти погоду города, поэтому выкидывает ошибку KeyError, которую я использую для построения
-#ответа пользователю
-
 def get_temp(response):
+    """Get a temperature from OWN reponse, or None if response is 'city wasn't found'"""
     try:
         temp = response["main"]["temp"]
     except KeyError:
@@ -19,8 +19,7 @@ def get_temp(response):
         return int(temp)
 
 def conv_kelv_to_cels(temp):
-    cels = int(temp - 273.15)
-    return round(cels)
+    return round(int(temp - 273.15))
 
 def add_C(cels):
-    return (str(cels) + u"\xb0C")
+    return str(cels) + u"\xb0C"
