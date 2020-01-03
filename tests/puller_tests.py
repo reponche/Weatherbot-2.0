61540f5e13f7c90377db564e20c65087ec130950
reponche <n.reponche@gmail.com>
2020-01-03 17:36:26 +0300

    Changed variables

--- tests/puller_tests.py
+++ tests/puller_tests.py
@@ -4,8 +4,7 @@ from weatherbot.puller import should_add, get_new
 class TestGetTemp(unittest.TestCase):
 
     def test_should_add_true(self):
-        a = should_add({"message": "Foo", "id": 3}, 1)
-        self.assertEqual(a, True)
+        self.assertEqual(should_add({"message": "Foo", "id": 3}, 1), True)
 
     def test_should_add_false(self):
         a = should_add({"message": "Foo", "id": 3}, 4)
@@ -13,9 +12,9 @@ class TestGetTemp(unittest.TestCase):
 
     def test_get_new(self):
         dict = [{"message": "/start","id": 1},{"message": "London, UK","id": 2},{"message": "/weather","id": 3},{"message": "message","id": 4},{"message": "Moscow","id": 5}]
-        self.assertEqual(get_new(dict, 2), '{"message": "/weather","id": 3} {"message": "message","id": 4} {"message": "Moscow","id": 5}')
+        test_res = '{"message": "/weather","id": 3} {"message": "message","id": 4} {"message": "Moscow","id": 5}'
+        self.assertEqual(get_new(dict, 2), test_res)
         self.assertEqual(get_new(dict, 5), None)
-
 
 if __name__ == "__main__" :
     unittest.main()
