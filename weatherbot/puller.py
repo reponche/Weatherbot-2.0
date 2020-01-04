9c195b63bf5e28ce6c60e35a399aa9bce471985f
reponche <n.reponche@gmail.com>
2020-01-05 01:03:04 +0300

    Deleted unnecessary var from test_add_in_queue

--- weatherbot/puller.py
+++ weatherbot/puller.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 def should_add(updates, update_id):
     return update_id < updates["update_id"]
 
@@ -10,7 +8,7 @@ def get_new(updates, update_id):
             buffer.append(item)
     return buffer
 
-def add_queue(queue, elems_list):
+def add_in_queue(queue, elems_list):
     for item in elems_list:
         queue.append(item)
-    return queue
+        pass
