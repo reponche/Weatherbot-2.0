23e4ec1db9eaca825a7f977e59b209b587810dde
reponche <n.reponche@gmail.com>
2020-01-08 01:50:01 +0300

    Added class Puller

--- weatherbot/puller.py
+++ weatherbot/puller.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def should_add(updates, update_id):
     return update_id < updates["update_id"]
 
@@ -8,7 +10,26 @@ def get_new(updates, update_id):
             buffer.append(item)
     return buffer
 
-def add_in_queue(queue, elems_list):
-    for item in elems_list:
-        queue.append(item)
-    return queue
+class Puller:
+    def __init__(self):
+        self.queue = deque()
+        self.update_id = 0
+
+    def pull(self, updates):
+        date = get_new(updates, self.update_id)
+        for elems in date:
+            self.queue.append()
+            self.update_id = elem["update_id"]
+
+    def get_elems(self):
+        buffer = []
+        while len(self.queue) > 0:
+            buffer.append(self.queue.pop())
+        return buffer
+
+    def get_state(self):
+        return self.queue
+
+a = Puller()
+print(a.get_elems())
+print(a.get_state())
