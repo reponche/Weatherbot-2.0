6c2fc270e77a96a9a99d012140a0877e42a1d455
reponche <n.reponche@gmail.com>
2020-02-22 23:57:31 +0300

    Added owm_results.json

--- weatherbot/map.py
+++ weatherbot/map.py
@@ -9,10 +9,13 @@ def get_coords(city):
     return coords_list
 
 
-def init_map():
-    coords_list = get_coords(city)
+def init_map(coords_list):
     m = folium.Map(location=[0, 0], zoom_start=2)
     folium.Marker(coords_list, popup=None).add_to(m)
     return m
+
+
+
+
 
 
